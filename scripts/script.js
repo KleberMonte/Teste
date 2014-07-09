7ae5297cba96e96dd33c7fc854e2d425964bafef
KleberMonte <klebermonte@outlook.com>
2014-07-08 23:11:26 -0300

    Adicao das pastas do projeto

---
+++ scripts/script.js
@@ -0,0 +1,13 @@
+document.getElementById("next").addEventListener("click", direita, false);
+
+function direita()
+{
+	alert("Direita");
+}
+
+function esquerda()
+{
+	alert("Esquerda");
+}
+
+//alteração feita ...
