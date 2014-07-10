e9789d42335ffa0906a304210a5ab48397d24b00
KleberMonte <klebermonte@outlook.com>
2014-07-10 00:32:20 -0300

    Alteracoes nas funcoes

--- scripts/script.js
+++ scripts/script.js
@@ -1,19 +1,53 @@
 document.getElementById("next").addEventListener("click", direita, false);
+document.getElementById("back").addEventListener("click", esquerda, false);
+
+var imgDirOld;
+var imgEsqOld;
+var x = getEsquerda(document.getElementById("img1"));
+var imagens = [];
+
+function getEsquerda(elemento)
+{
+	var teste = elemento.offsetLeft;
+
+	while((elemento = elemento.offsetParent) != null)
+		teste += elemento.offsetLeft;
+
+	return teste;
+}
 
 function direita()
 {
-	alert("Direita");
+
+	alert(x);
+
+	if(imgDirOld != document.getElementById("img1").src)
+		imgDirOld = document.getElementById("img1").src;
+
+	if(x > -200)
+	{
+		x--;
+		document.getElementById("img1").left = x;		
+	}
+
+	if(x == -200)
+		document.getElementById("img1").src = "Imagens/ImagensBanner/2.jpg";
+
+	var intervalo = setTimeout(direita(), 100);
 }
 
 function esquerda()
 {
-	alert("Esquerda");
+	if(imgDEsqOld != document.getElementById("img1").src)
+		imgDEsqOld = document.getElementById("img1").src;
+
+	document.getElementById("img1").src = imgDirOld;	
 }
 
-function teste(texto)
+function carregarImagens() 
 {
-	alert(texto);
+	for (var i = 0; i < 3 ; i++) 
+	{
+		
+	}
 }
-
-
-teste ("Alteração zar ...");
